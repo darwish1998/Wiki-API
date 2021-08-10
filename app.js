3cc6a705c3e172f5b7a83d64389413f917df82a9
darwish144.ad <darwish144.ad@gmail.com>
2021-08-10 21:53:46 +0300

    File set up for API creation

---
+++ app.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB"{useNewUrlParser: true});
+
+const articalSchema {
+  title: String,
+  content: String
+};
+const Artical = mongoose.model("Artical",articalSchema);
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
